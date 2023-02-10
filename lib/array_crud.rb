baaa92b5414812018914babdbf1e84d5568c815c
tugi <blueqwinn@gmail.com>
2023-02-10 20:27:41 +0300

    first commit

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,68 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  student age[18]
+  height [168]
+  engine [ 500]
+  index_number [14]
 end
 
 def add_element_to_end_of_array(array, element)
+#Adding an element to end of array
+  a = Array.new(array , element)
+
+    syntax:Array.append(array, element)
+    parameter: element. to_param
+    Return: array of elements
   
 end
 
 def add_element_to_start_of_array(array, element)
-  
+# Adding element to start of array
+  int arr[5] ={1, 2, 3, 4,5};
+  int arr[5] = {1, 2, 3};
+  #Valid ..
+
 end
 
 def remove_element_from_end_of_array(array)
+#Removing element from end of array
+b = Array.new (eat , cook , sleep , cleanup , go to market)
+  Array.delete( cleanup , got to market)
   
 end
 
 def remove_element_from_start_of_array(array)
-  
+#Removing element from start of array
+ c = Array.new (0, 1, 2 ,3, 4 ,5 , 6, 7, ,8, 9, 10)
+  Array.delete_at(0 , 1 ,2 , 3 ,4 , 5)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  # Retrieving element from index
+  d= Array.new (18 ,22 ,33 , nil , 5 ,6)
+
+  Array.retrieve_element_from_index(5, 6)
 end
 
 def retrieve_first_element_from_array(array)
-  
+# Retreiving the first element from the array
+ e=  Array.new(sit , stand , move left , move right ,talk , keep quiet)
+
+ 1.Syntax.Array.first( sit)
+ 2.Parameter:Array.n- no. of elements in Array
+ 3.Return:first element of the array or the first 'n' elements from the array.
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+# Retreive the last element of the array
+f = Array.new( Bathe , Eat , Sleep , WakeUp)
+
+  Syntax.Array.last(WakeUp)
+  Parameter:Array n-no.of elements 
+  Return /; last element of the array or the last 'n' elements from the array
+
 end
